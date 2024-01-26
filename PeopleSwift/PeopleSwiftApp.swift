ac30eb99eca32732173140c8b357a110879785ba
martin-ngigi <martinwainaina001@gmail.com>
2024-01-26 07:58:04 +0300

    Initial commit

---
+++ PeopleSwift/PeopleSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleSwiftApp.swift
+//  PeopleSwift
+//
+//  Created by Martin Wainaina on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PeopleSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
